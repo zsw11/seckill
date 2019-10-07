65d01809f35726701eec76da7baf4b910299a975
zsw11 <546232194@qq.com>
2019-10-07 16:40:03 +0800

    test

--- src/main/java/com/dayup/seckil/base/controller/BaseApiController.java
+++ src/main/java/com/dayup/seckil/base/controller/BaseApiController.java
@@ -2,7 +2,9 @@ package com.dayup.seckil.base.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
+ * test
+ */
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", allowCredentials = "true")
 public class BaseApiController {
